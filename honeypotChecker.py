781d9785a918cd8e73af5c816a8eb44fbdb659c3
Treading-Tigers <79805603+Trading-Tiger@users.noreply.github.com>
2021-09-06 19:24:53 +0200

    Update HoneypotChecker & txns.py

--- honeypotChecker.py
+++ honeypotChecker.py
@@ -1,20 +1,15 @@
-from requests_html import HTMLSession
-import time 
-
+import requests, json
 
 class HoneyPotChecker():
-
     def __init__(self, Token_Address):
-        self.url = f"https://ishoneypot.trading-tigers.com/?address={Token_Address}"
-        self.session = HTMLSession()
+        self.url = f"https://ishoneypot.trading-tigers.com/token/{Token_Address}"
         self.Honeypot = True
-
+        
     def Is_Honeypot(self):
-        r = self.session.get(self.url)
-        r.html.render(sleep=0.5, keep_page=True)
-        p = r.html.xpath('//*[@id="checktoken"]/div')[0].text
-        if "Does not seem like a honeypot." in p:
+        r = requests.get(self.url)
+        jres = json.loads(r.text)
+        if jres['HONEYPOT']  == False:
             return False
-        else:
-            self.Honeypot = True
-            return self.Honeypot
+        elif jres['HONEYPOT'] == True:
+            return True
+
