c813a53485f12d8be440c22e0e5a7a759b264f04
Treading-Tigers <79805603+Trading-Tiger@users.noreply.github.com>
2021-09-21 21:40:45 +0200

    Update && add Featuresq

--- Sniper.py
+++ Sniper.py
@@ -6,48 +6,71 @@ from honeypotChecker import HoneyPotChecker
 from halo import Halo
 from threading import Thread
 
-
-gas_price = 6 * (10**9)
-swap = [1]
-TIGS = Txn_bot(token_address="0x34faa80fec0233e045ed4737cc152a71e490e2e3", quantity=0, slippage=0, gas_price=gas_price, swap=swap)
-TIGSBALANCE = TIGS.get_token_balance()
-print("")
-print("")
-if TIGSBALANCE >= 100:
-    Time = 0.1
-    print("Welcome Premium Tiger!\nSet Checktime to " + str(Time) + " seconds")
-else:
-    Time = 5
-    print("Welcome Tiger Buy 100TIGS to get faster checktime.\nSet Checktime to " + str(Time) + " seconds")
-del TIGS, TIGSBALANCE
-
-
-parser = argparse.ArgumentParser(description='Set your Token and Amount example: "sniper.py -t 0xc87b88aafb95f0b88c3a74fc96344e4bccab6bde -a 0.2 -s 15"')
-parser.add_argument('-t', '--token', help='str, Token for snipe e.g. "-t 0xc87b88aafb95f0b88c3a74fc96344e4bccab6bde"')
+parser = argparse.ArgumentParser(description='Set your Token and Amount example: "sniper.py -t 0x34faa80fec0233e045ed4737cc152a71e490e2e3 -a 0.2 -s 15"')
+parser.add_argument('-t', '--token', help='str, Token for snipe e.g. "-t 0x34faa80fec0233e045ed4737cc152a71e490e2e3"')
 parser.add_argument('-a', '--amount', help='float, Amount in Bnb to snipe e.g. "-a 0.1"')
 parser.add_argument('-s', '--slippage', default=10, nargs="?", const=1, type=int, help='int, slippage in % "-s 10"')
 parser.add_argument('-tx', '--txamount', default=1, nargs="?", const=1, type=int, help='int, how mutch tx you want to send? It Split your BNB Amount in e.g. "-tx 5"')
+parser.add_argument('-hp', '--honeypot', default=True, nargs="?", const=True, type=bool, help='bool, check if your token to buy is a Honeypot, e.g. "-hp True"')
 parser.add_argument('-swap', '--swap', default=[1], type=list, help='list, Witch Swap? e.g. "-swap [1] for Panackeswap"')
-parser.add_argument('-hp', '--honeypot', default=True, nargs="?", const=True, type=bool, help='bool, check if your token to buy is a Honeypot')
+parser.add_argument('-tp', '--takeprofit', default=0, nargs="?", const=True, type=int, help='int, Percentage TakeProfit from your input BNB amount, if 0 then not used. e.g. "-tp 50" ')
+parser.add_argument('-wb', '--awaitBlocks', default=0, nargs="?", const=True, type=int, help='int, Await Blocks bevore sending BUY Transaction, if 0 then not used. e.g. "-ab 50" ')
 args = parser.parse_args()
+
+
+
+
 
 
 TXN = int(args.txamount)
 SNIPEquantity = (float(args.amount) / TXN)
 slippage = int(args.slippage)
 token=args.token
-checkHoney = args.honeypot
+checkHoney = bool(args.honeypot)
+swap = args.swap
+takeprofit = int(args.takeprofit)
+waitingBlocks = int(args.awaitBlocks)
+gas_price = 6 * (10**9)
+
+TIGS = Txn_bot(token_address="0x34faa80fec0233e045ed4737cc152a71e490e2e3", quantity=0, slippage=slippage, gas_price=gas_price, swap=swap)
+TIGSBALANCE = TIGS.get_token_balance()
+
+ 
+print("")
+if TIGSBALANCE >= 100:
+    Time = 0.1
+    print("Welcome Premium Tiger!\nSet Checktime to " + str(Time) + " seconds")
+else:
+    Time = 5
+    print("Welcome Tiger Buy 100 TIGS to get faster checktime.\nSet Checktime to " + str(Time) + " seconds")
+
 Timer = float(Time)
+
+def calcProfitExit():
+    a = ((SNIPEquantity * TXN) * takeprofit) / 100
+    b = a + (SNIPEquantity * TXN)
+    return b 
+    
+profit = "not activated"
+if takeprofit != 0: 
+    profit = calcProfitExit() 
+else:
+    profit = 0
+del TIGS, TIGSBALANCE
+
 
 print("")
 print("Start Sniper with following arguments")
-print("------------------------------------------------------------------------------")
+print("---------------------------------------------------")
 print("Amount for Buy:", str(args.amount) , "BNB")
 print("Token to Snipe :", args.token)
 print("Slippage :", str(args.slippage) + "%")
 print("Transaction to send:", str(args.txamount))
 print("Amount per transaction :", str(SNIPEquantity))
-print("------------------------------------------------------------------------------")
+print("Await Blocks before buy :", str(waitingBlocks))
+print("Take Profit Percent :", str(takeprofit))
+print("Min Output from Take Profit:",str(profit))
+print("---------------------------------------------------")
 
 
 def checkIsHoneypot():
@@ -55,42 +78,110 @@ def checkIsHoneypot():
     return isHoney
 
 
-def buy():
-    sleep(0.1)
-    print("\nOK, Send", TXN, "Transaction ,Good luck")
-    for i in range(TXN):
-        try:
-            bot = Txn_bot(token_address=token, quantity=SNIPEquantity, slippage=slippage, gas_price=gas_price, swap=swap)
-            bot.buy_token()
-        except Exception as e:
-            print(e)
-    
-
-@Halo(text='Waiting for Liquidity', spinner='dots')
-def Snip():
-    sbot = Txn_bot(token_address=token, quantity=SNIPEquantity, slippage=slippage, gas_price=gas_price, swap=swap)
-    while(True):
+def checkProfit():
+    spinner = Halo(text='Checking Profit', spinner='dots')
+    spinner.start()
+    pbot = Txn_bot(
+                    token_address=token,
+                    quantity=SNIPEquantity,
+                    slippage=slippage,
+                    gas_price=gas_price,
+                    swap=swap
+                    )
+    pbot.approve()
+    tq = pbot.get_token_balance()
+    cbot = Txn_bot(
+                    token_address=token,
+                    quantity=tq,
+                    slippage=slippage,
+                    gas_price=gas_price,
+                    swap=swap
+                    )
+    tokenQuantity = tq / TXN
+    #print("Waiting for Profit...")
+    while True:
         sleep(Timer)
         try:
-            print(f"\nMin Output from {SNIPEquantity} BNB:",sbot.amountsOut_buy()[1] / (10 ** sbot.get_token_decimals()))
-            try:
+            currentProfit = (cbot.amountsOut_sellWithoutSlipp()[1] /(10**18))
+            #print("\nCurrent Output from your Tokens", round(currentProfit,4), end="\r")
+            if currentProfit >= profit:
+                #print("\nProfit reached, Sell now all Tokens.")
+                spinner.stop()
+                for i in range(TXN):
+                    sbot = Txn_bot(token_address=token, quantity=tokenQuantity, slippage=slippage, gas_price=gas_price, swap=swap)
+                    sbot.sell_token()
+                break
+        except Exception as e:
+            print(e)
+            break
+
+          
+def waitBlocks():
+    spinner = Halo(text='Waiting Blocks', spinner='dots')
+    spinner.start()
+    blocksbot = Txn_bot(token_address=token, quantity=0, slippage=0, gas_price=gas_price, swap=swap)
+    waitForHigh = int(blocksbot.getBlockHigh()) + waitingBlocks
+    while True:
+        try:
+            sleep(0.8)
+            currentBlock = blocksbot.getBlockHigh()
+            if waitForHigh <= currentBlock:
                 if checkHoney == True:
                     T = checkIsHoneypot()
                 else:
                     T = False
                 if T == False:
-                    print("\nOK, your token is no honeypot!")
-                    Thread(target=buy,).start()
+                    print("\nOK, your Token is not a honeypot!")
+                    spinner.stop()
+                    buy()
                     break
                 else:
-                    print("\n",token, "is Honeypot Token!")
+                    print("\n",token, "is current a Honeypot Token!")
                     break
+        except Exception as e:
+            print(e)
+            break
+
+
+
+def buy():
+    spinner = Halo(text='BUY Tokens', spinner='dots')
+    spinner.start()
+    sleep(0.1)
+    try:
+        print("\nOK, BUY with", TXN, "Transactions, Good luck")
+        for i in range(TXN):
+            try:
+                bot = Txn_bot(token_address=token, quantity=SNIPEquantity, slippage=slippage, gas_price=gas_price, swap=swap)
+                bot.buy_token()
+            except Exception as e:
+                print(e)
+        spinner.stop()
+        if profit != 0:
+            checkProfit()
+    except Exception as e:
+        print(e)
+    
+
+
+def Snip():
+    spinner = Halo(text='Waiting for Liquidity', spinner='dots')
+    spinner.start()
+    sbot = Txn_bot(token_address=token, quantity=SNIPEquantity, slippage=slippage, gas_price=gas_price, swap=swap)
+    while True:
+        sleep(Timer)
+        try:
+            print(f"\nMin Output from {SNIPEquantity} BNB:",sbot.amountsOut_buy()[1] / (10 ** sbot.get_token_decimals()))
+            try:
+                spinner.stop()
+                waitBlocks()
+                break
             except Exception as e:
                 print(e)
                 break
         except:
             pass
-
+    
 
 Snip()
 
--- fastBuy.py
+++ fastBuy.py
@@ -7,25 +7,33 @@ parser = argparse.ArgumentParser(description='Set your Token and Amount example:
 parser.add_argument('-t', '--token', help='str, Token for snipe e.g. "-t 0xc87b88aafb95f0b88c3a74fc96344e4bccab6bde"')
 parser.add_argument('-a', '--amount', help='float, Amount in Bnb to snipe e.g. "-a 0.1"')
 parser.add_argument('-s', '--slippage', help='int, slippage in % "-s 10"')
+parser.add_argument('-hp', '--honeypot', default=True, nargs="?", const=True, type=bool, help='bool, check if your token to buy is a Honeypot')
+parser.add_argument('-swap', '--swap', default=[1], type=list, help='list, Witch Swap? e.g. "-swap [1] for Panackeswap"')
+parser.add_argument('-tx', '--txamount', default=1, nargs="?", const=1, type=int, help='int, how mutch tx you want to send? It Split your BNB Amount in e.g. "-tx 5"')
 args = parser.parse_args()
 
 
 quantity = float(args.amount)
 slippage = int(args.slippage)
+checkHoney = bool(args.honeypot)
 gas_price = 6 * (10**9) 
-swap = [1]
-token_address=args.token
+swap = list(args.swap)
+token_address = str(args.token)
+TXN = int(args.txamount)
 
 
-bot = Txn_bot(
-    token_address=token_address,
-    quantity=quantity,
-    slippage=slippage,
-    gas_price=gas_price,
-    swap=swap)
+if checkHoney == True:
+    isHoneypot = HoneyPotChecker(Token_Address=token_address).Is_Honeypot()
+    if isHoneypot == False:
+        BUY = True
+if checkHoney == False:
+    BUY = True
 
 
-if HoneyPotChecker(Token_Address=token_address).Is_Honeypot() == False:
-    bot.buy_token()
+if BUY = True:
+    quantity = quantity / TXN
+    for i in range(TXN):
+        bot = Txn_bot(token_address=token_address, quantity=quantity, slippage=slippage,gas_price=gas_price,swap=swap)
+        bot.buy_token()
 else:
     print("Token is Hoooneypooot!")
