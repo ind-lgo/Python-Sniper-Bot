653f1f64f4121d2fbfda5a1bfb9db752cfb5d16d
trading-tiger <Seven7Sev7@protonmail.com>
2021-11-27 14:32:59 +0100

    Adding New Feature: Trailing-Stop-Loss

--- README.md
+++ README.md
@@ -40,21 +40,28 @@ python Sniper.py -t <TOKEN_ADDRESS> -a <AMOUNT> -tx <TXAMOUNT> -hp -wb <BLOCKS W
 python Sniper.py -t 0x34faa80fec0233e045ed4737cc152a71e490e2e3 -a 0.001 -tx 2 -hp  -wb 10 -tp 50
 python Sniper.py -t 0x34faa80fec0233e045ed4737cc152a71e490e2e3 --sellonly
 python Sniper.py -t 0x34faa80fec0233e045ed4737cc152a71e490e2e3 -a 0.001 --buyonly
+python Sniper.py -t 0x34faa80fec0233e045ed4737cc152a71e490e2e3 -tsl 10 -nb
 ```  
 
 Here are all options with infos:  
 ```python3
-*'-t' or '--token', Token for snipe e.g. "-t 0xc87b88aafb95f0b88c3a74fc96344e4bccab6bde"
+*'-t' or '--token', Token for snipe e.g. "-t 0x34faa80fec0233e045ed4737cc152a71e490e2e3"
 '-a' or '--amount', float, Amount in Bnb to snipe e.g. "-a 0.1"
 
 '-tx' or '--txamount', how mutch tx you want to send? It split your BNB amount in e.g. "-tx 5"
-'-hp' or '--honeypot', if you use this Flag, your token get checks if token is honypot before buy!
-'-tp' or '--takeprofit', default=0,  Percentage TakeProfit from your input BNB amount. e.g. "-tp 50" 
 
 '-wb' or '--awaitBlocks', default=0, Await Blocks before sending BUY Transaction. e.g. "-ab 50" 
+
+'-hp' or '--honeypot', if you use this Flag, your token get checks if token is honypot before buy!
+
+'-nb' or '--nobuy', No Buy, Skipp buy, if you want to use only TakeProfit/StopLoss/TrailingStopLoss
+'-tp' or '--takeprofit', Percentage TakeProfit from your input BNB amount. e.g. "-tp 50" 
+'-tsl'or '--trailingstoploss', 'Percentage Trailing-Stop-loss from your first Quote "-tsl 50"
 
 '-so' or '--sellonly', Sell ALL your Tokens from given token address
 '-bo' or '--buyonly', Buy Tokens with your given amount
 
 * = require every time its runs!
 ```
+## New Feature, Trailing-Stop-Loss:
+<img src="https://i.ytimg.com/vi/dZFb0-fwqOk/maxresdefault.jpg" height="400">
--- Sniper.py
+++ Sniper.py
@@ -18,8 +18,10 @@ parser.add_argument('-t', '--token', help='str, Token for snipe e.g. "-t 0x34faa
 parser.add_argument('-a', '--amount',default=0, help='float, Amount in Bnb to snipe e.g. "-a 0.1"')
 parser.add_argument('-tx', '--txamount', default=1, nargs="?", const=1, type=int, help='int, how mutch tx you want to send? It Split your BNB Amount in e.g. "-tx 5"')
 parser.add_argument('-hp', '--honeypot', action="store_true", help='Check if your token to buy is a Honeypot, e.g. "-hp" or "--honeypot"')
+parser.add_argument('-nb', '--nobuy', action="store_true", help='No Buy, Skipp buy, if you want to use only TakeProfit/StopLoss/TrailingStopLoss')
 parser.add_argument('-tp', '--takeprofit', default=0, nargs="?", const=True, type=int, help='int, Percentage TakeProfit from your input BNB amount "-tp 50" ')
-parser.add_argument('-sl', '--stoplose', default=0, nargs="?", const=True, type=int, help='int, Percentage Stop lose from your input BNB amount "-sl 50" ')
+parser.add_argument('-sl', '--stoploss', default=0, nargs="?", const=True, type=int, help='int, Percentage Stop loss from your input BNB amount "-sl 50" ')
+parser.add_argument('-tsl', '--trailingstoploss', default=0, nargs="?", const=True, type=int, help='int, Percentage Trailing-Stop-loss from your first Quote "-tsl 50" ')
 parser.add_argument('-wb', '--awaitBlocks', default=0, nargs="?", const=True, type=int, help='int, Await Blocks before sending BUY Transaction "-ab 50" ')
 parser.add_argument('-so', '--sellonly',  action="store_true", help='Sell all your Tokens from given address')
 parser.add_argument('-bo', '--buyonly',  action="store_true", help='Buy Tokens with from your given amount')
@@ -51,8 +53,8 @@ class SniperBot():
             print(style().YELLOW + "Take Profit Percent :",style().GREEN + str(self.tp)+ style().RESET)
             print(style().YELLOW + "Target Output for Take Profit:",style().GREEN +str("{0:.8f}".format(self.takeProfitOutput))+ style().RESET)
         if self.sl != 0:
-            print(style().YELLOW + "Stop Lose Percent :",style().GREEN + str(self.sl)+ style().RESET)
-            print(style().YELLOW + "Sell if Output is smaller as:",style().GREEN +str("{0:.8f}".format(self.stoplose))+ style().RESET)
+            print(style().YELLOW + "Stop loss Percent :",style().GREEN + str(self.sl)+ style().RESET)
+            print(style().YELLOW + "Sell if Output is smaller as:",style().GREEN +str("{0:.8f}".format(self.stoploss))+ style().RESET)
         print(style().BLUE + "---------------------------------"+ style().RESET)
         
     def parseArgs(self):
@@ -61,34 +63,41 @@ class SniperBot():
             print(style.RED+"Please Check your Token argument e.g. -t 0x34faa80fec0233e045ed4737cc152a71e490e2e3")
             print("exit!")
             sys.exit()
-        self.amount = args.amount  
-        if not args.sellonly: 
-            if self.amount == 0:
-                print(style.RED+"Please Check your Amount argument e.g. -a 0.01")
-                print("exit!")
-                sys.exit()
+        self.amount = args.amount
+        if args.nobuy != True:  
+            if not args.sellonly: 
+                if self.amount == 0:
+                    print(style.RED+"Please Check your Amount argument e.g. -a 0.01")
+                    print("exit!")
+                    sys.exit()
         self.tx = args.txamount
         self.amountForSnipe = float(self.amount) / float(self.tx)
         self.hp = args.honeypot
         self.wb = args.awaitBlocks
         self.tp = args.takeprofit
-        self.sl = args.stoplose 
-        self.stoplose = 0
+        self.sl = args.stoploss 
+        self.tsl = args.trailingstoploss
+        self.stoploss = 0
         self.takeProfitOutput = 0
         if self.tp != 0:
             self.takeProfitOutput = self.calcProfit()
         if self.sl != 0:
-            self.stoplose = self.calcLose()
+            self.stoploss = self.calcloss()
 
     def calcProfit(self):
         a = ((self.amountForSnipe * self.tx) * self.tp) / 100
         b = a + (self.amountForSnipe * self.tx)
         return b 
     
-    def calcLose(self):
+    def calcloss(self):
         a = ((self.amountForSnipe * self.tx) * self.sl) / 100
         b = (self.amountForSnipe * self.tx) - a
         return b 
+
+    def calcNewTrailingStop(self, currentPrice):
+        a = (currentPrice  * self.tsl) / 100
+        b = currentPrice - a
+        return b
 
     def getTaxHoneypot(self):
         url = f"https://ishoneypot.trading-tigers.com/token/{self.token}"
@@ -121,6 +130,7 @@ class SniperBot():
         if tx[0] != True:
             sys.exit() 
 
+
     def awaitApprove(self):
         spinner = Halo(text='await Approve', spinner=spinneroptions)
         spinner.start()
@@ -130,6 +140,7 @@ class SniperBot():
         print(tx[1])
         if tx[0] != True:
             sys.exit() 
+
 
     def awaitBlocks(self):
         spinner = Halo(text='await Blocks', spinner=spinneroptions)
@@ -142,6 +153,7 @@ class SniperBot():
                 break
         print(style().GREEN+"[DONE] Wait Blocks finish!")
         
+
     def awaitLiquidity(self):
         spinner = Halo(text='await Liquidity', spinner=spinneroptions)
         spinner.start()
@@ -159,27 +171,42 @@ class SniperBot():
                 continue
         print(style().GREEN+"[DONE] Liquidity is Added!"+ style().RESET)
     
-    def awaitProfitLose(self):
+
+    def awaitProfitloss(self):
         TokenBalance = round(self.TXN.get_token_balance(),5)
         while True:
             sleep(0.3)
             Output = float(self.TXN.getOutputfromTokentoBNB() / (10**18))
             print(f"Token Balance: {TokenBalance} current output:", "{0:.8f}".format(Output), end="\r")
-
             if self.takeProfitOutput != 0:
                 if Output >= self.takeProfitOutput:
                     print()
                     print(style().GREEN+"[TAKE PROFIT] Triggert!"+ style().RESET)
                     self.awaitSell()
                     break
-
-            if self.stoplose != 0:
-                if Output <= self.stoplose:
+            if self.stoploss != 0:
+                if Output <= self.stoploss:
                     print()
-                    print(style().GREEN+"[STOP LOSE] Triggert!"+ style().RESET)
+                    print(style().GREEN+"[STOP LOSS] Triggert!"+ style().RESET)
                     self.awaitSell()
                     break
-        print(style().GREEN+"[DONE] TakeProfit/StopLose Finished!"+ style().RESET)
+        print(style().GREEN+"[DONE] TakeProfit/StopLoss Finished!"+ style().RESET)
+
+    def awaitTrailingStopLoss(self):
+        highestLastPrice = 0
+        while True:
+            sleep(0.3)
+            LastPrice = float(self.TXN.getOutputfromTokentoBNB() / (10**18))
+            if LastPrice > highestLastPrice:
+                highestLastPrice = LastPrice
+                TrailingStopLoss = self.calcNewTrailingStop(LastPrice)
+            if LastPrice < TrailingStopLoss:
+                print(style().GREEN+"[TRAILING STOP LOSS] Triggert!"+ style().RESET)
+                self.awaitSell()
+                break
+            print("Sell below","{0:.8f}".format(TrailingStopLoss),"| CurrentOutput:", "{0:.8f}".format(LastPrice), end="\r")
+        print(style().GREEN+"[DONE] TrailingStopLoss Finished!"+ style().RESET)
+
 
     def StartUP(self):
         self.TXN = TXN(self.token, self.amountForSnipe)
@@ -195,7 +222,8 @@ class SniperBot():
             print(f"Start BuyOnly, buy now with {self.amountForSnipe}BNB tokens!")
             print(self.TXN.buy_token()[1])
             sys.exit()
-        self.awaitLiquidity()
+        if args.nobuy != True:
+            self.awaitLiquidity()
         honeyTax = self.getTaxHoneypot()
         if honeyTax[1] > self.settings["MaxSellTax"]:
             print(style().RED+"Token SellTax exceeds Settings.json, exiting!")
@@ -212,10 +240,13 @@ class SniperBot():
                 print(style().GREEN +"[DONE] Token is NOT a Honeypot!" + style().RESET)
         if self.wb != 0: 
             self.awaitBlocks()
-        self.awaitBuy()
+        if args.nobuy != True:
+            self.awaitBuy()
         self.awaitApprove()
-        if self.stoplose != 0 or self.takeProfitOutput != 0:
-            self.awaitProfitLose()
+        if self.tsl != 0:
+            self.awaitTrailingStopLoss()
+        if self.stoploss != 0 or self.takeProfitOutput != 0:
+            self.awaitProfitloss()
         print(style().GREEN + "[DONE] TradingTigers Sniper Bot finish!" + style().RESET)
 
 
