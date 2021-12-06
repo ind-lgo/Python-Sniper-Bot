e02cbcf1f185bdd86c5823ff4a6f9f94c7ed2433
Trading-Tigers <79805603+Trading-Tiger@users.noreply.github.com>
2021-12-06 19:43:27 +0100

    new contract, outsourced api to blockchain!

--- txns.py
+++ txns.py
@@ -6,13 +6,13 @@ from style import style
 class TXN():
     def __init__(self, token_address, quantity):
         self.w3 = self.connect()
-        self.address, self.private_key = self.set_address()
+        self.address, self.private_key = self.setup_address()
         self.token_address = Web3.toChecksumAddress(token_address)
-        self.token_contract = self.set_token_contract()
-        self.router_address, self.router = self.set_router()
-        self.slippageBUY, self.slippageSELL = self.setSlippage()
+        self.token_contract = self.setup_token()
+        self.swapper_address, self.swapper = self.setup_swapper()
+        self.slippageBUY, self.slippageSELL = self.setupSlippage()
         self.quantity = quantity 
-        self.gas_price = self.setGas()
+        self.gas_price = self.setupGas()
 
     def connect(self):
         with open("./Settings.json") as f:
@@ -23,12 +23,12 @@ class TXN():
             w3 = Web3(Web3.HTTPProvider(keys["RPC"]))
         return w3
 
-    def setGas(self):
+    def setupGas(self):
         with open("./Settings.json") as f:
             keys = json.load(f)
         return int(keys['GAS'] * (10**9))
 
-    def set_address(self):
+    def setup_address(self):
         with open("./Settings.json") as f:
             keys = json.load(f)
         if len(keys["metamask_address"]) <= 41:
@@ -37,7 +37,7 @@ class TXN():
             print(style.RED +"Set your PrivateKey in the keys.json file!"+ style.RESET)
         return keys["metamask_address"], keys["metamask_private_key"]
 
-    def setSlippage(self):
+    def setupSlippage(self):
         with open("./Settings.json") as f:
             keys = json.load(f)
         return keys['MaxBuyTax'], keys['MaxSellTax']
@@ -48,14 +48,14 @@ class TXN():
     def getBlockHigh(self):
         return self.w3.eth.block_number
 
-    def set_router(self):
-        router_address = Web3.toChecksumAddress("0x9dAa730D7dD1b911923B89d236FDFF834FBF71Ae") 
+    def setup_swapper(self):
+        swapper_address = Web3.toChecksumAddress("0x1Ba0c582354c628Ab621b1bd0328528Fbb8d80fC") 
         with open("./abis/BSC_Swapper.json") as f:
             contract_abi = json.load(f)
-        router = self.w3.eth.contract(address=router_address, abi=contract_abi)
-        return (router_address, router)
+        swapper = self.w3.eth.contract(address=swapper_address, abi=contract_abi)
+        return (swapper_address, swapper)
 
-    def set_token_contract(self):
+    def setup_token(self):
         with open("./abis/bep20_abi_token.json") as f:
             contract_abi = json.load(f)
         token_contract = self.w3.eth.contract(address=self.token_address, abi=contract_abi)
@@ -63,6 +63,17 @@ class TXN():
 
     def get_token_balance(self): 
         return self.token_contract.functions.balanceOf(self.address).call() / (10 ** self.token_contract.functions.decimals().call())
+
+    def checkToken(self):
+        tokenInfos = self.swapper.functions.getTokenInfos(self.token_address).call()
+        print(tokenInfos)
+        buy_tax = round((tokenInfos[0] - tokenInfos[1]) / tokenInfos[0] * 100) 
+        sell_tax = round((tokenInfos[2] - tokenInfos[3]) / tokenInfos[2] * 100)
+        if tokenInfos[5] and tokenInfos[6] == True:
+            honeypot = False
+        else:
+            honeypot = True
+        return buy_tax, sell_tax, honeypot
 
     def estimateGas(self, txn):
         gas = self.w3.eth.estimateGas({
@@ -74,7 +85,7 @@ class TXN():
         return gas
 
     def getOutputfromBNBtoToken(self):
-        call = self.router.functions.getOutputfromBNBtoToken(
+        call = self.swapper.functions.getOutputfromBNBtoToken(
             self.token_address,
             int(self.quantity * (10**18)),
             ).call()
@@ -83,7 +94,7 @@ class TXN():
         return Amount, Way
 
     def getOutputfromTokentoBNB(self):
-        call = self.router.functions.getOutputfromTokentoBNB(
+        call = self.swapper.functions.getOutputfromTokentoBNB(
             self.token_address,
             int(self.token_contract.functions.balanceOf(self.address).call()),
             ).call()
@@ -93,7 +104,7 @@ class TXN():
 
     def buy_token(self):
         self.quantity = self.quantity * (10**18)
-        txn = self.router.functions.fromBNBtoToken(
+        txn = self.swapper.functions.fromBNBtoToken(
             self.token_address,
             self.slippageBUY
         ).buildTransaction(
@@ -115,7 +126,7 @@ class TXN():
         else: return False, style.RED +"\nBUY Transaction Faild!" + style.RESET
 
     def is_approve(self):
-        Approve = self.token_contract.functions.allowance(self.address ,self.router_address).call()
+        Approve = self.token_contract.functions.allowance(self.address ,self.swapper_address).call()
         Aproved_quantity = self.token_contract.functions.balanceOf(self.address).call()
         if int(Approve) <= int(Aproved_quantity):
             return False
@@ -125,7 +136,7 @@ class TXN():
     def approve(self):
         if self.is_approve() == False:
             txn = self.token_contract.functions.approve(
-                self.router_address,
+                self.swapper_address,
                 115792089237316195423570985008687907853269984665640564039457584007913129639935 # Max Approve
             ).buildTransaction(
                 {'from': self.address, 
@@ -149,7 +160,7 @@ class TXN():
 
     def sell_tokens(self):
         self.approve()
-        txn = self.router.functions.fromTokentoBNB(
+        txn = self.swapper.functions.fromTokentoBNB(
             self.token_address,
             int(self.token_contract.functions.balanceOf(self.address).call()),
             self.slippageSELL
@@ -170,7 +181,3 @@ class TXN():
         txn_receipt = self.w3.eth.waitForTransactionReceipt(txn)
         if txn_receipt["status"] == 1: return True,style.GREEN +"\nSELL Transaction Successfull!" + style.RESET
         else: return False, style.RED +"\nSELL Transaction Faild!" + style.RESET
-
-
-
-
